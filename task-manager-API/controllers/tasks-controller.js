d8b1e643fa6283eaaa24108054894fa5899ef0d6
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-01 21:54:35 +0300

    started on routes

--- task-manager-API/controllers/tasks-controller.js
+++ task-manager-API/controllers/tasks-controller.js
@@ -1,5 +1,19 @@
 const getAllTasks = (req, res) => {
   res.send(`all taks`);
 };
+const createTask = (req, res) => {
+  res.json(req.body);
+  // res.send(`create task`);
+};
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+  // res.send(`Get one task`);
+};
+const updateTask = (req, res) => {
+  res.send(`update task`);
+};
+const deleteTask = (req, res) => {
+  res.send(`delete task`);
+};
 
-module.exports = { getAllTasks };
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
